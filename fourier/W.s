d596fec0c7cc3f923bb293f4fec4f1d85066a134
Joey Thornberry <git@joeythornberry.com>
2026-01-08 09:06:56 -0500

    calculate W using cosine and sin of remainder * 2pi

--- fourier/W.s
+++ fourier/W.s
@@ -2,11 +2,27 @@
 .type 	W "function"
 W:
 	/* x0: integer exponent */
-	stp 	x29, x30, [sp, #-16]!
-
-	fmov 	s0, 2.0
-	ucvtf 	s1, x0
-	bl powf
+	/* x1: N */
 	
-	ldp 	x29, x30, [sp], #-16
+	stp 	x29, x30, [sp, -16]!
+	
+	ldr	s1, =0x40c90fda	/* 2pi */
+ 
+	ucvtf 	s10, x0		/* load integer params into floating point registers */
+	ucvtf 	s0, x1
+	fdiv 	s10, s10, s0 	/* divide exponent by N */
+	frinta 	s0, s10		/* round result to nearest integer */
+	fsub 	s10, s10, s0 	/* subtract rounded from original, leaving residue */
+	
+	fmul 	s10, s10, s1 	/* now multiply residue by 2pi, forming theta */
+	
+	fmov 	s0, s10		/* take sine of theta */
+	bl 	sine
+	fmov 	s11, s0
+
+	fmov 	s0, s10		/* take cosine of theta */
+	bl 	cosine
+	fmov 	s1, s11
+
+	ldp	x29, x30, [sp], 16
 	ret
--- fourier/test.c
+++ fourier/test.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 #include <assert.h>
 
+typedef struct Complex {
+	float a;
+	float b;
+} Complex;
+
 extern float sine(float x);
 extern float cosine(float x);
+extern Complex W(int exponent, int N);
 
 int floatcmp(float a, float b) {
-	printf("Comparing %f and %f\n", a, b);
+	//printf("Comparing %f and %f\n", a, b);
 	float c = a - b;
 	if (c < 0) c *= -1.0f;
-	return c < 0.01;
+	return c < 0.001;
+}
+
+int complexcmp(Complex c, float a, float b) {
+	printf("Comparing %f + %fi and %f + %fi\n", c.a, c.b, a, b);
+	return floatcmp(c.a, a) && floatcmp(c.b, b);
 }
 
 int main() {
 	printf("TESTING SINE\n");
-	float sinevals[] = { 0.0f,0.0, 1.5707796f,1.0f, -1.5707796f,-1.0f, 0.3154f,0.311147f, 0.5f,0.479425 };
+	float sinevals[] = { 0.0f,0.0, 1.5707796f,1.0f, -1.5707796f,-1.0f, 0.3154f,0.311147f, 3.11f,0.0315873f  };
 	for (int i = 0; i < 10; i += 2) {
 		assert(floatcmp(sine(sinevals[i]),sinevals[i+1]));
 	}
 
 	printf("TESTING COSINE\n");
-	float cosinevals[] = { 0.0f,1.0f, 1.5707796f,0.0000167f, -1.5707796f,0.0000167f, 0.2135f,0.977295, 0.5f,0.877582 };
+	float cosinevals[] = { 0.0f,1.0f, 1.5707796f,0.0000167f, -1.5707796f,0.0000167f, 0.2135f,0.977295, 3.11f,-0.999501 };
 	for (int i = 0; i < 10; i += 2) {
 		assert(floatcmp(cosine(cosinevals[i]),cosinevals[i+1]));
 	}
+	
+	printf("TESTING W\n");
+	assert(complexcmp(W(8,5), -0.809107f, -0.587785f));
 	
 	return 0;
 }
